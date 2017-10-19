fa068a47e07e4e53b40738f2014a4da3077372f8
Lex Myers <lex.myers@gmail.com>
2017-10-19 15:11:38 -0700

    added first unit tests

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -1,14 +1,33 @@
 using System;
 using Xunit;
+using starwars;
+
 
 namespace test
 {
     public class UnitTest1
     {
+        Sith helmet = new Sith("Vader");
+        Jedi obiwan = new Jedi("Ben Kenobi");
         [Fact]
-        public void Test1()
+        public void TestSithCreation()
         {
-
+            Assert.Equal(helmet.name, "Darth Vader");
+            Assert.Equal(helmet.health, 50);
+            Assert.Equal(helmet.strength, 5);
+            Assert.Equal(helmet.intelligence, 10);
+            Assert.Equal(helmet.stealth, 0);
+            Assert.Equal(helmet.agility, 0);
+        }
+        [Fact]
+        public void TestJediCreation()
+        {
+            Assert.Equal(obiwan.name, "Ben Kenobi");
+            Assert.Equal(obiwan.health, 50);
+            Assert.Equal(obiwan.strength, 5);
+            Assert.Equal(obiwan.intelligence, 10);
+            Assert.Equal(obiwan.stealth, 0);
+            Assert.Equal(obiwan.agility, 0);
         }
     }
 }
